ae336f1c78ee7490e6ba403deb6f076924382776
jgd10 <jamesgordonderrick@gmail.com>
2022-02-12 16:44:23 +0000

    added new grain library example

---
+++ examples/grain_library_showcase.py
@@ -0,0 +1,15 @@
+from PySALESetup import GrainLibrary
+import matplotlib.pyplot as plt
+
+
+grain_library = GrainLibrary()
+grain_library.load_builtin_library()
+fig = plt.figure()
+
+for i in range(1, 25+1):
+    ax = fig.add_subplot(5, 5, i)
+    grain = grain_library.random_grain()
+    grain.plot(ax)
+
+fig.tight_layout()
+plt.show()
