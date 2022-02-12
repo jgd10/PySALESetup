a1fe9e72c686a2c026a625a1ca4cadfdb577b732
jgd10 <jamesgordonderrick@gmail.com>
2022-02-12 15:24:40 +0000

    corrected examples for new behaviour

--- examples/extension_zones.py
+++ examples/extension_zones.py
@@ -12,14 +12,13 @@ from PySALESetup.mesh import ExtensionZone, Region, \
 import matplotlib.pyplot as plt
 
 # Construct 4 extension zones: one for each region
-extension_zones = [ExtensionZone(10, region)
+extension_zones = [ExtensionZone(15, region, 1.,
+                                 ExtensionZoneFactor(1.05, 20))
                    for region in [Region.NORTH, Region.SOUTH,
                                   Region.EAST, Region.WEST]]
-extension_factor = ExtensionZoneFactor(1.05, 20)
 
 # Build a mesh using the extension zones
 m = PySALEMesh(100, 100, extension_zones=extension_zones,
-               extension_factor=extension_factor,
                cell_size=1.)
 
 object1 = PySALEObject.generate_ellipse([56, 56],
@@ -31,14 +30,11 @@ object2 = PySALEObject.generate_ellipse([159, 159],
 object3 = PySALEObject.generate_ellipse([107.2, 107.2],
                                         50., 50., material=3,
                                         rotation=0)
-object4 = PySALEObject([(0, 150), (205, 150), (205, 205), (0, 205)])
-object4.set_material(4)
 
 
 m.apply_geometry(object1)
 m.apply_geometry(object2)
 m.apply_geometry(object3)
-m.apply_geometry(object4)
 
 f, ax = m.plot_materials()
 # NB x_range and y_range are cell centres, not cell edges!
