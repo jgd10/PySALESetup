4d2a0c348212e21d9e71465601e75c68fc4dc403
jgd10 <jamesgordonderrick@gmail.com>
2025-01-26 00:36:09 +0000

    Deploying to gh-pages from @ jgd10/PySALESetup@7f436d5b37ca0a36ef9a7ace8fdb2b632297b0ad 🚀

--- _downloads/cb49ec3b54851e28ff96799b47c05179/extension_zones.py
+++ _downloads/cb49ec3b54851e28ff96799b47c05179/extension_zones.py
@@ -24,6 +24,11 @@ extension_zones = [ExtensionZone(15, region, 1.,
 m = PySALEMesh(100, 100, extension_zones=extension_zones,
                cell_size=1.)
 
+####################################################
+# We create a dummy host to help visualise the problem, but we won't use it.
+#
+
+host = PySALEObject([(-150, -150), (-150, 250), (250, 250), (250, -150)])
 object1 = PySALEObject.generate_ellipse([56, 56],
                                         50., 50., material=1,
                                         rotation=0)
@@ -33,12 +38,37 @@ object2 = PySALEObject.generate_ellipse([159, 159],
 object3 = PySALEObject.generate_ellipse([107.2, 107.2],
                                         50., 50., material=3,
                                         rotation=0)
+object4 = PySALEObject.generate_ellipse([0, 0],
+                                        50., 50., material=4,
+                                        rotation=0)
+object5 = PySALEObject.generate_ellipse([-56, -56],
+                                        50., 50., material=5,
+                                        rotation=0)
+object6 = PySALEObject.generate_ellipse([-56, 159],
+                                        50., 50., material=6,
+                                        rotation=0)
+object7 = PySALEObject.generate_ellipse([159, -56],
+                                        50., 50., material=7,
+                                        rotation=0)
+for ob in [object1, object2, object3, object4, object5, object6, object7]:
+    host.add_child(ob)
 
+host.plot()
+plt.show()
 
-m.project_polygons_onto_mesh([object1, object2, object3])
+####################################################
+# Project the circles onto our new mesh and plot the result.
+#
+
+m.project_polygons_onto_mesh(host.children)
 
 f, ax = m.plot_materials()
 # NB x_range and y_range are cell centres, not cell edges!
+
+####################################################
+# This final step adds lines to cell centres, which makes it easier to see
+# how the extension zone regions work.
+#
 for item in m.x_range:
     ax.axvline(x=item, lw=0.2, color='m')
 for item in m.y_range:
